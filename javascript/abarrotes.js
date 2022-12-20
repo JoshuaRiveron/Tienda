8a8e54c3ea4d372550f7b0aafc48678526bb6088
JoshuaRiveron <jriveronm2000@alumno.ipn.mx>
2022-12-19 19:55:33 -0600

    Tienda

---
+++ javascript/abarrotes.js
@@ -0,0 +1,32 @@
+const productos = [
+    {nombre: 'Maruchan', valor: 17},
+]
+
+const formulario = document.querySelector('#formulario');
+const boton = document.querySelector('#boton');
+const resultado = document.querySelector('#resultado');
+
+const filtrar = () => {
+    //console.log(formulario.value);
+    resultado.innerHTML = '';
+
+    const texto = formulario.value.toLowerCase();
+    for(let producto of productos){
+        let nombre = producto.nombre.toLowerCase();
+        if(nombre.indexOf(texto) !== -1 ){
+            resultado.innerHTML += `
+                <li>${producto.nombre} - Precio: ${producto.valor}</li>
+            `
+        }
+    }
+    if(resultado.innerHTML === ''){
+        resultado.innerHTML += `
+            <li>Producto no encontrado</li>
+        `
+    }
+}
+
+boton.addEventListener('click', filtrar)
+formulario.addEventListener('keyup', filtrar)
+
+filtrar();
